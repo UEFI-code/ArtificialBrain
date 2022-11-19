bff130a7801a7ffa18ee1102c7eed23c4a2f1943
UEFI-code <1092700291@qq.com>
2022-11-19 10:22:57 +0000

    Add myHippo to the Brain!

--- CenterNN.py
+++ CenterNN.py
@@ -2,8 +2,10 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import time
+import myHippo
+
 class CenterNN(nn.Module):
-    def __init__(self, num, exciterRatio):
+    def __init__(self, num, exciterRatio, memDepth, memWidth):
         super(CenterNN, self).__init__()
         self.fc = nn.Linear(num, num)
         weight = self.fc.weight.detach()
@@ -14,19 +16,40 @@ class CenterNN(nn.Module):
         self.nNum = num - self.pNum
         self.dataBuf = torch.randn(1, num)
         self.relu = nn.ReLU()
-        print(weight)
+        #print(weight)
+        self.hippo = myHippo.myHippo(memDepth, memWidth)
+        self.divToP = int(memWidth * exciterRatio)
+        self.divToN = memWidth - self.divToP
+    
+    def MindArea(self, x):
+        x = self.fc(x)
+        cut1 = x[:, :self.pNum]
+        cut2 = x[:, self.pNum:]
+        cut1 = self.relu(cut1)
+        cut2 = self.relu(cut2 * -1) * -1
+        x = torch.cat((cut1, cut2), 1)
+        return x
+    
+    def HippoInteface(self, x):
+        cut1 = x[:, 0:self.divToP]
+        cut2 = x[:, self.pNum:self.pNum+self.divToN]
+        y = torch.cat((cut1, cut2), 1)
+        y = self.hippo.inferencer(y[0]).view(1, self.divToP+self.divToN)
+        print('Hippo Output: ', y)
+        cut1 = y[:, :self.divToP]
+        cut2 = y[:, self.divToP:]
+        x[:, self.divToP:self.divToP*2] = cut1
+        x[:, self.pNum+self.divToN:self.pNum+self.divToN*2] = cut2
+        return x
 
     def forward(self):
         while True:
-            #print(self.dataBuf)
-            self.dataBuf = self.fc(self.dataBuf)
-            cut1 = self.dataBuf[:, :self.pNum]
-            cut2 = self.dataBuf[:, self.pNum:]
-            cut1 = self.relu(cut1)
-            cut2 = self.relu(cut2 * -1) * -1
-            self.dataBuf = torch.cat((cut1, cut2), 1)
+            print(self.dataBuf)
+            x = self.MindArea(self.dataBuf)
+            x = self.HippoInteface(x)
+            self.dataBuf = x
             time.sleep(1)
     
 if __name__ == '__main__':
-    net = CenterNN(10, 0.5)
+    net = CenterNN(10, 0.5, 4, 4)
     net.forward()
---
+++ myHippo.py
@@ -0,0 +1,58 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import random
+
+class myHippo(nn.Module):
+    def __init__(self, poolSize, poolDim):
+        super(myHippo, self).__init__()
+        self.poolDim = poolDim
+        self.memPool = []
+        for i in range(poolSize):
+            self.memPool.append(torch.randn(poolDim))
+    
+    def resonancer(self, x):
+        outputBuffer = torch.zeros(self.poolDim)
+        for i in self.memPool:
+            simliar = F.cosine_similarity(x.view(1, self.poolDim), i.view(1, self.poolDim)) #Note for contrary Memory
+            outputBuffer += simliar * i
+            i += simliar * x
+            if(i.abs().max() != 0):
+                i /= i.abs().max()
+        return outputBuffer / outputBuffer.abs().max()
+    
+    def curiouser(self, x):
+        newMemID = 0
+        levelP = 0
+        levelN = 0
+        for i in range(len(self.memPool)):
+            simliar = F.cosine_similarity(x.view(1, self.poolDim), self.memPool[i].view(1, self.poolDim))
+            if simliar > 0: #Positive Memory
+                levelP += simliar
+            elif simliar <= 0: #Negative Memory
+                levelN += simliar
+            if self.memPool[i].abs().sum() < self.memPool[newMemID].abs().sum(): #Found New Memory Index
+                newMemID = i
+        #levelP = 1 / levelP
+        levelN = -1 * levelN
+        print('Familiar level Positive: ', levelP)
+        print('Curious level Negative: ', levelN)
+        levelFin = levelN - levelP
+        print('Curious level Final: ', levelFin)
+        print('Choose new memory ID: ', newMemID)
+        self.memPool[newMemID] += x * levelFin
+        if(self.memPool[newMemID].abs().max() != 0):
+            self.memPool[newMemID] /= self.memPool[newMemID].abs().max()
+    
+    def inferencer(self, x):
+        decide = random.randint(0, 1)
+        if decide == 0:
+            res = self.resonancer(x)
+            self.curiouser(x)
+        else:
+            self.curiouser(x)
+            res = self.resonancer(x)
+        return res
+
+           
+
