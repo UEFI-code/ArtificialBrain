e8866485c4b4345394209315f027d70f2201081d
UEFI-code <1092700291@qq.com>
2022-12-02 09:27:40 +0000

    Add Parall

--- CenterNN.py
+++ CenterNN.py
@@ -3,9 +3,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 import time
 import myHippo
+from threading import Thread
 
-enableHippoA = False
-enableHippoB = True
+enableHippoI1 = False
+enableHippoI2 = True
 
 debug = True
 
@@ -24,8 +25,9 @@ class CenterNN(nn.Module):
         self.relu = nn.ReLU()
         #print(weight)
         self.hippo = myHippo.myHippo(memDepth, memWidth)
-        self.divToP = int(memWidth * exciterRatio)
-        self.divToN = memWidth - self.divToP
+        self.hippoBuf = None
+        # self.divToP = int(memWidth * exciterRatio)
+        # self.divToN = memWidth - self.divToP
 
         self.advanceHippoInputWarpper = nn.Linear(num, memWidth)
         self.advanceHippoOutputWarpper = nn.Linear(memWidth, num)
@@ -43,46 +45,71 @@ class CenterNN(nn.Module):
         x = torch.cat((cut1, cut2), 1)
         return x
     
-    def HippoInteface(self, x):
-        assert(self.divToP * 2 <= self.pNum)
-        assert(self.divToN * 2 <= self.nNum)
-        assert(self.divToP + self.divToN == self.hippo.poolDim)
+    # def HippoInteface(self, x):
+    #     assert(self.divToP * 2 <= self.pNum)
+    #     assert(self.divToN * 2 <= self.nNum)
+    #     assert(self.divToP + self.divToN == self.hippo.poolDim)
 
-        cut1 = x[:, 0:self.divToP]
-        cut2 = x[:, self.pNum:self.pNum+self.divToN]
-        y = torch.cat((cut1, cut2), 1)
-        y = self.hippo.inferencer(y[0]).view(1, self.divToP+self.divToN)
-        if debug:
-            print('Hippo Output: ', y)
-            self.fHippoIO.write(str(y))
-        cut1 = y[:, :self.divToP]
-        cut2 = y[:, self.divToP:]
-        x[:, self.divToP:self.divToP*2] = cut1
-        x[:, self.pNum+self.divToN:self.pNum+self.divToN*2] = cut2
-        return x
+    #     cut1 = x[:, 0:self.divToP]
+    #     cut2 = x[:, self.pNum:self.pNum+self.divToN]
+    #     y = torch.cat((cut1, cut2), 1)
+    #     y = self.hippo.inferencer(y[0]).view(1, self.divToP+self.divToN)
+    #     cut1 = y[:, :self.divToP]
+    #     cut2 = y[:, self.divToP:]
+    #     x[:, self.divToP:self.divToP*2] = cut1
+    #     x[:, self.pNum+self.divToN:self.pNum+self.divToN*2] = cut2
+    #     return x
 
     def HippoInterface2(self, x):
         x = self.advanceHippoInputWarpper(x)
         x = self.hippo.inferencer(x[0]).view(1, self.hippo.poolDim)
-        if debug:
-            print('Hippo Output: ', x)
-            self.fHippoIO.write(str(x))
         x = self.advanceHippoOutputWarpper(x)
         return x
 
-    def forward(self):
+    def forwardTestLoop(self):
         while True:
-            if debug:
-                print(self.dataBuf)
-                self.fNeuro.write(str(self.dataBuf))
-            x = self.MindArea(self.dataBuf)
-            if enableHippoA:
-                x = self.HippoInteface(x)
-            if enableHippoB:
-                x = self.HippoInterface2(x)
-            self.dataBuf = x
-            time.sleep(1)
+            self.dataBuf = self.MindArea(self.dataBuf)
+            print(self.dataBuf)
+            self.fNeuro.write(str(self.dataBuf))
+            # if enableHippoI1:
+            #     self.hippoBuf = self.HippoInteface(self.dataBuf)
+            #     print('HippoBuf: ', self.hippoBuf)
+            #     self.fHippoIO.write(str(self.hippoBuf))
+            #     self.dataBuf += self.hippoBuf
+            if enableHippoI2:
+                self.hippoBuf = self.HippoInterface2(self.dataBuf)
+                print('HippoBuf: ', self.hippoBuf)
+                self.fHippoIO.write(str(self.hippoBuf))
+                self.dataBuf += self.hippoBuf
+            time.sleep(3)
+    
+    def forwardCoreLoop(self):
+        while True:
+            self.dataBuf = self.MindArea(self.dataBuf)
+            time.sleep(3)
+    
+    # def forwardHippoI1(self):
+    #     while True:
+    #         self.hippoBuf = self.HippoInteface(self.dataBuf)
+    #         self.dataBuf += self.hippoBuf
+    
+    def forwardHippoI2(self):
+        while True:
+            self.hippoBuf = self.HippoInterface2(self.dataBuf)
+            self.dataBuf += self.hippoBuf
+            time.sleep(3)
     
 if __name__ == '__main__':
     net = CenterNN(16, 0.6, 4, 4)
-    net.forward()
+    #net.forwardTestLoop()
+    t1 = Thread(target=net.forwardCoreLoop)
+    t2 = Thread(target=net.forwardHippoI2)
+    t1.start()
+    t2.start()
+    while True:
+        if debug:
+            print('MindArea: ', net.dataBuf)
+            net.fNeuro.write(str(net.dataBuf))
+            print('Hippo Output: ', net.hippoBuf)
+            net.fHippoIO.write(str(net.hippoBuf))
+        time.sleep(3)   
